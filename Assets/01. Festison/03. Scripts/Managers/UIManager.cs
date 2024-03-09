49826c6d172086c5ccc7e167e1ec0b6f7562b222
Festison <sundogdog1958@gmail.com>
2024-03-09 22:54:52 +0900

    UI줌인 동적 스케일 변경 (03-09)

--- Assets/01. Festison/03. Scripts/Managers/UIManager.cs
+++ Assets/01. Festison/03. Scripts/Managers/UIManager.cs
@@ -2,28 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : SingleTon<UIManager>
 {
-    [Header("첫 클릭 확인 UI")] public GameObject dotUi;
+    // [Header("첫 클릭 확인 UI")] public GameObject dotUi;
     [Header("메뉴 UI")] public GameObject menu;
-    private bool isSetmenu=false;
+    [Header("메뉴 UI 버튼")] public Button menuBtn;
+    [SerializeField] private bool isSetmenu = false;
 
-    public void OnClickDot()
+    public void OnClickMenu()
     {
-        dotUi.SetActive(false);
+        //dotUi.SetActive(false);
 
         if (!isSetmenu)
         {
+            menuBtn.enabled = false;
             menu.SetActive(true);
-            isSetmenu = true;
+            menu.transform.DOScale(1.0f, 0.5f).OnComplete(SetActiveMenu);           
         }
-        else
+        else if (isSetmenu)
         {
-            menu.SetActive(false);
-            isSetmenu = false;
+            menuBtn.enabled = false;
+            menu.transform.DOScale(0f, 0.5f).OnComplete(SetFalseMenu);
         }
-        
     }
 
+    public void SetActiveMenu()
+    {
+        isSetmenu = true;
+        menuBtn.enabled = true;
+    }
+    public void SetFalseMenu()
+    {
+        menu.SetActive(false);
+        isSetmenu = false;
+        menuBtn.enabled = true;
+    }
 }
