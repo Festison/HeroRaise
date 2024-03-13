00ed61615e3d8cfc69b3a78c3c0b9629f324d6dd
Festison <105289311+Festison@users.noreply.github.com>
2024-03-13 15:18:40 +0900

    스킬 버튼 클릭시 스킬 사용 구현 (03-13)

---
+++ Assets/01. Festison/03. Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+namespace Festioson
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private SkeletonAnimation skeletonAnimation;
+
+        private void Start()
+        {
+            skeletonAnimation = GetComponent<SkeletonAnimation>();
+        }
+        public void SkillAnimation()
+        {
+            skeletonAnimation.AnimationName = "skill";
+            Invoke("DefalutAniamtion", 2.167f);
+        }
+
+        public void DefalutAniamtion()
+        {
+            skeletonAnimation.AnimationName = "attack";
+        }
+    }
+}
+
