12ffbba414ed5e36d5af3f434404a1c793b1bb83
vecolius <dydwk1105@naver.com>
2024-03-19 15:02:45 +0900

    03.19 Wave 두번째 업데이트
    1. 웨이브 시작 시, 일정 주기마다 몬스터 spawn

--- Assets/03. Vecolius/1. Script/Manager/WaveManager.cs
+++ Assets/03. Vecolius/1. Script/Manager/WaveManager.cs
@@ -31,24 +31,45 @@ namespace Veco
 
         public int monsterSpawnCount;
         public int waveCount;
+        [SerializeField] bool isWaveOn;
+        [SerializeField] float spawnCooltime;
 
         [SerializeField] Transform spawnPos;
         [SerializeField] GameObject spiderObj;
+        GameObject spawnMonster;
+
+        IEnumerator spawnCo;
+        WaitForSeconds waitforSecond;
         void Start()
         {
             wave = new Wave(waveInfos);
+            waitforSecond = new WaitForSeconds(spawnCooltime);
+            spawnCo = Spawn(spiderObj);
+
             Invoke("WaveStart", 2.0f);
+            //StartCoroutine(spawnCo);
         }
 
         void Update()
         {
-
+            if (Input.GetKey(KeyCode.Space))
+                spawnMonster.GetComponent<MonsterAI>().Hit(100);
         }
 
         public void WaveStart()
         {
             //GameObject spawnMonster = ObjectPoolManager.instance.PopObj(wave.waveMonsterInfo[waveCount].monsterName);
-            GameObject spawnMonster = ObjectPoolManager.instance.PopObj(spiderObj, spawnPos.position, Quaternion.identity);
+            //spawnMonster = ObjectPoolManager.instance.PopObj(spiderObj, spawnPos.position, Quaternion.identity);
+            StartCoroutine(spawnCo);
+        }
+
+        IEnumerator Spawn(GameObject monsterObj)
+        {
+            while (isWaveOn)
+            {
+                ObjectPoolManager.instance.PopObj(spiderObj, spawnPos.position, Quaternion.identity);
+                yield return waitforSecond;
+            }
         }
     }
 }
